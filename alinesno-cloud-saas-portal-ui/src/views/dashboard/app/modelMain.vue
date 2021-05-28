1c58556cf241afa64e467d6b1420eae9138f9626
landonniao@163.com <landonniao@163.com>
2021-05-28 08:47:34 +0800

    feat: 添加配置和功能教程

--- alinesno-cloud-saas-portal-ui/src/views/dashboard/app/modelMain.vue
+++ alinesno-cloud-saas-portal-ui/src/views/dashboard/app/modelMain.vue
@@ -1,12 +1,84 @@
 <template>
-<div class="container-main">
-</div>
+    <div class="container-main">
+        <div class="inie71-0 bTZuBz">
+            <div class="sc-1u7nlc0-0 kEITgy">
+                <div class="tqojp3-0 hGylxJ">
+                    <span class="header-title">Your complete app building platform</span>
+                    <span class="header-subtitle"> Ionic Framework is 100% free and open source. Ionic Appflow is a free tool that help </span>
+                </div>
+                <div class="account-type-selector">
+                    <ul>
+                        <li class="">
+                            <div class="account-type-item">
+                                <div class="plan-info">
+                                    <div class="type">
+                                        Hobby
+                                    </div>
+                                    <ul class="features">
+                                        <li>Manage app projects in the Ionic <strong>cloud dashboard</strong></li>
+                                        <li>Access and post questions on Ionic <strong>Forum</strong></li>
+                                        <li>Deploy <strong>100 live updates</strong> / mo</li>
+                                        <li>Community support</li>
+                                    </ul>
+                                </div>
+                                <button class="aap711-0 LrZpX choose-btn  dash-color-primary dash-size-regular" font-weight="700"><span>Choose Hobby</span></button>
+                            </div></li>
+                        <li class="">
+                            <div class="account-type-item">
+                                <div class="plan-info">
+                                    <div class="type">
+                                        Launch
+                                    </div>
+                                    <ul class="features">
+                                        <li>All <strong>Hobby</strong> plan features, plus:</li>
+                                        <li><strong>Publish directly to app stores</strong> from your cloud dashboard</li>
+                                        <li>Deploy <strong>10K live updates</strong> / mo</li>
+                                        <li>Compile <strong>native app binaries</strong></li>
+                                    </ul>
+                                </div>
+                                <button class="aap711-0 LrZpX choose-btn   dash-color-primary dash-size-regular" font-weight="700"><span>Selected</span></button>
+                            </div></li>
+                        <li class="">
+                            <div class="account-type-item">
+                                <div class="plan-info">
+                                    <div class="type">
+                                        Growth
+                                    </div>
+                                    <ul class="features">
+                                        <li>All <strong>Launch</strong> plan features</li>
+                                        <li><strong>Collaborate</strong> with team members and clients</li>
+                                        <li><strong>CI/CD automation</strong> to ship continuously with mobile</li>
+                                        <li>Deploy <strong>25K live updates</strong> / mo</li>
+                                    </ul>
+                                </div>
+                                <button class="aap711-0 LrZpX choose-btn  dash-color-primary dash-size-regular" font-weight="700"><span>Choose Growth</span></button>
+                            </div></li>
+                        <li>
+                            <div class="account-type-item">
+                                <div class="plan-info">
+                                    <div class="type">
+                                        Scale
+                                    </div>
+                                    <ul class="features">
+                                        <li>Use with other CI/CD Tools</li>
+                                        <li>All <strong>Growth</strong> plan features, plus:</li>
+                                        <li>Scale to millions of users with <strong>custom Deploy limits</strong></li>
+                                        <li>iOS <strong>Enterprise builds</strong></li>
+                                    </ul>
+                                </div>
+                                <button class="aap711-0 LrZpX choose-btn dash-color-primary dash-size-regular" font-weight="700"><span>Contact us</span></button>
+                            </div></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 </script>
 
 <style scoped lang="scss">
-@import '@/resources/css/build/overview.scss';
+@import '@/resources/css/build/model.scss';
 </style>
 
