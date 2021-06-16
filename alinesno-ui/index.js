bcf80cc5d9cdbd1c2af56343051932633447ecc6
luoandon@gmail.com <luoandon@gmail.com>
2021-06-16 21:29:49 +0800

    feat:封装前端字典组件

--- alinesno-ui/index.js
+++ alinesno-ui/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 
-import Layout from './packages/Layout/layout.vue'
-import Login from './packages/Login/Login.vue'
+import Layout from './packages/Layout/layout'
+import Login from './packages/Login/Login'
+import DictSelect from './packages/Dictionary/index'
 
 export {
   Layout ,
+  DictSelect ,
   Login
 }
