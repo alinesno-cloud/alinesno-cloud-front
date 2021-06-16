82a042393bdfc0516c692373883c6ad51a17f8d6
luoandon@gmail.com <luoandon@gmail.com>
2021-06-16 16:40:51 +0800

    feat:升级版本为1.3.3

--- alinesno-ui/packages/Layout/components/TopHeader/application.vue
+++ alinesno-ui/packages/Layout/components/TopHeader/application.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="select-app">
-    <el-select v-model="value" placeholder="请选择">
+    <el-select v-if="applicationList.length > 0" v-model="value" placeholder="请选择">
     <el-option
-    v-for="item in cities"
-    :key="item.value"
-    :label="item.label"
-    :value="item.value">
+        v-for="item in applicationList"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
     <span style="float: left">{{ item.label }}</span>
     <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
     </el-option>
@@ -17,19 +17,7 @@
   export default {
     data() {
       return {
-        cities: [{
-          value: 'Beijing',
-          label: '通用权限管理组件'
-        }, {
-          value: 'Shanghai',
-          label: '集团权限管理组件'
-        }, {
-          value: 'Nanjing',
-          label: '单点登陆管理平台'
-        }, {
-          value: 'Chengdu',
-          label: '消息通知管理平台'
-        }],
+        applicationList : [],
         value: ''
       }
     }
