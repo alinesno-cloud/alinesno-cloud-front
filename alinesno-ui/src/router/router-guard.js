1fc58b56901f9a1348617b28977598d34f9d5c4e
zhuoguangmign <zhuoguangming@linesno.com>
2021-07-09 13:56:19 +0800

    update

--- alinesno-ui/src/router/router-guard.js
+++ alinesno-ui/src/router/router-guard.js
@@ -1,5 +1,6 @@
 import { getToken, setExpiresIn, setRefreshToken, setToken } from '../utils/auth'
-import store from '../store'
+import { MessageBox } from 'element-ui'
+import request from '../utils/request'
 
 const routerTokenService = (to, from, next) => {
   const {
@@ -37,16 +38,48 @@ const routerTokenService = (to, from, next) => {
 
 }
 
-export const UaaRouterGuard = new Object()
+const AuthGuardService = (to, from, next) => {
+  if (!getToken()) {
+    //没有token时拦截
+    next(false)
+    MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
+        confirmButtonText: '重新登录',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }
+    ).then(() => {
+      request({
+        url: '/tryLogin',
+        method: 'get',
+      }).then((res) => {
+        const { loginEndPoint, secret } = res.data
+        const fromRouter = location.href
+        const query = new URLSearchParams()
+        query.append('fromHere', fromRouter)
+        query.append('secret', secret)
+        location.href = `${loginEndPoint}?${query.toString()}`
+      })
+    })
+  } else {
+    next()
+  }
+}
+
+const RouterGuardService = (to, from, next) => {
+
+
+}
+export const EnableUaaClient = new Object()
 /**
  * 接入全局的单点路由守卫,拦截所有的401响应和 beforeEach
  * @param Vue vue构造器
  * @param options {opts}
  */
-UaaRouterGuard.install = (Vue, options) => {
+EnableUaaClient.install = (Vue, options) => {
   if (options && options['routerInstance']) {
     const { routerInstance } = options
     routerInstance.beforeEach(routerTokenService)
+    routerInstance.beforeEach(AuthGuardService)
   } else {
     console.warn('UaaRouterGuard', '使用UaaRouterGuard插件必须为options设置路由实例对象!!!')
   }
--- alinesno-ui/src/utils/request.js
+++ alinesno-ui/src/utils/request.js
@@ -15,7 +15,9 @@ const service = axios.create({
 // request拦截器
 service.interceptors.request.use(config => {
   // 是否需要设置 token
-  config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+  if (config.url !== '/tryLogin') {
+    config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+  }
 
   // get请求映射params参数
   if (config.method === 'get' && config.params) {
