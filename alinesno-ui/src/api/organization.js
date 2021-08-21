2e1b15c39fd1b54d90544e0aaf91031b1d44fff1
wufei@linesno.com <wufei@linesno.com>
2021-08-21 17:08:10 +0800

    feat：整合组织机构、人选弹窗，并调整样式，将数据本地化

--- alinesno-ui/src/api/organization.js
+++ alinesno-ui/src/api/organization.js
@@ -1,1 +1,24 @@
+// import { get } from '@/axios'
+// const BASE_URL = "/packages/Organization/mockData/"
+// // 获取组织机构根节点
+// export const GET_DEPT_ROOT = () => get( BASE_URL + 'depRoot.json' )
 
+// // 根据部门id获取分页人员信息
+// export const GET_PAGE_EMPLOYEE = data => get( BASE_URL + 'userData.json', data )
+
+// // 获取组织机构子节点
+// export const GET_DEPT_TREE = data => get( BASE_URL + 'depChild.json', data )
+
+// // 获取组织机构下人员信息
+// export const GET_USER_BY_DEPT = data => get( BASE_URL + 'userData.json', data )
+
+// 从正式借口获取后
+import request from 'alinesno-ui/src/utils/request'
+// 获取组织机构根节点
+export const GET_DEPT_ROOT = () => {return request({url: 'depRoot.json',method: 'get'})}
+// 根据部门id获取分页人员信息
+export const GET_PAGE_EMPLOYEE = () => {return request({url: 'userData.json',method: 'get'})}
+// 获取组织机构子节点
+export const GET_DEPT_TREE = () => {return request({url: 'depChild.json',method: 'get'})}
+// 获取组织机构下人员信息
+export const GET_USER_BY_DEPT = () => {return request({url: 'userData.json',method: 'get'})}
